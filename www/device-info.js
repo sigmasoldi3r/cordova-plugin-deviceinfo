314c554662cab5237510336f7a72779d422bd81d
Pablo Blanco Celdran <pablobc.1995@gmail.com>
2016-09-19 10:22:54 +0200

    Revert "Added ready function (isReady(); === true|false)"
    This reverts commit 843bfbce4a0dfddb1ab39d4513fd0a6edbdcd7d3.
    
    # Conflicts:
    #	www/device-info.js

---
+++ www/device-info.js
@@ -0,0 +1,47 @@
+/*
+     Android DeviceInfo - An Android Miscellaneous device information provider
+    Copyright (C) 2016 Pablo 'sigmasoldi3r' Blanco Celdrán
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	
+	Contact: pablobc.1995@gmail.com
+*/
+cordova.define("cordova-plugin-deviceinfo.deviceInfo", function (require, exports, module) {
+
+    module.exports = [
+        'getBoard',
+        'getBrand',
+        'getDevice',
+        'getDisplay',
+        'getHost',
+        'getId',
+        'getManufacturer',
+        'getModel',
+        'getProduct',
+        'getTags',
+        'getType',
+        'getUser',
+        'getUID',
+        'getSwDpi',
+        'getSdkVersion',
+        'getOsVersion'
+    ].reduce(function (a, v) {
+        var b = a;
+        b[v] = function (name, successCallback, errorCallback) {
+            cordova.exec(successCallback, errorCallback, "DeviceInfoProvider", v, []);
+        };
+        return b;
+    }, {});
+
+});
